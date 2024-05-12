bc79f923651b5b13c0e2037af81d715187c1d17c
@robinlant <mtarasovets08@gmail.com>
2024-05-12 14:27:36 +0200

    modify main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-        source = "hashicorp/google"
+        source  = "hashicorp/google"
         version = "4.51.0"
     }
   }
@@ -9,9 +9,35 @@ terraform {
 
 provider "google" {
   project = "t-diagram-422507-k9"
-  region = "europe-west10-an"
+  region  = "europe-west10-a"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+resource "google_compute_instance" "server" {
+  name         = "instance-20240509-084748"
+  machine_type = "e2-medium"
+  zone         = "europe-west10-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12-bookworm-v20240415"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    subnetwork = "default"
+
+    access_config {
+      nat_ip       = "34.32.26.88"
+      network_tier = "PREMIUM"
+    }
+  }
+
+  service_account {
+    email  = "github-actions-deploy@t-diagram-422507-k9.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+
+
+  tags = ["http-server", "https-server"]
 }
